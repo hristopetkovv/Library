383f2a29a6bc04c56768771e7109ba661235cfbc
Hristo Petkov <hristopetkov161@gmail.com>
2026-09-17 14:25:10 +0300

    Change test method for get book by id

--- Library.Application.Tests/Books/Queries/GetBookByIdQueryHandlerTests.cs
+++ Library.Application.Tests/Books/Queries/GetBookByIdQueryHandlerTests.cs
@@ -7,19 +7,31 @@ public class GetBookByIdQueryHandlerTests
         BookMappingConfig.Configure();
     }
     private readonly Mock<IBookRepository> bookRepo;
+    private readonly Mock<IUserRepository> userRepo;
     private readonly Mock<IUnitOfWork> unitOfWork;
+    private readonly Mock<IUserContext> userContext;
     private readonly GetBookByIdQueryHandler handler;
+
+    private const int TestUserId = 42;
 
     public GetBookByIdQueryHandlerTests()
     {
         bookRepo = new Mock<IBookRepository>();
+        userRepo = new Mock<IUserRepository>();
         unitOfWork = new Mock<IUnitOfWork>();
+        userContext = new Mock<IUserContext>();
+
         unitOfWork.Setup(u => u.Books).Returns(bookRepo.Object);
-        handler = new GetBookByIdQueryHandler(unitOfWork.Object);
+        unitOfWork.Setup(u => u.Users).Returns(userRepo.Object);
+        userContext.Setup(c => c.UserId).Returns(TestUserId);
+
+        handler = new GetBookByIdQueryHandler(unitOfWork.Object, userContext.Object);
     }
 
-    [Fact]
-    public async Task Handle_ShouldReturnBookDetailDto_WhenBookExists()
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task Handle_ShouldReturnBookDetailDto_WhenBookExists(bool expectedIsFavorite)
     {
         var book = BookFactory.Create(1, "Test Book", 1, 1, "1234567890");
         var query = new GetBookByIdQuery(1);
@@ -30,11 +42,15 @@ public class GetBookByIdQueryHandlerTests
                 It.IsAny<Expression<Func<Book, object>>[]>()))
             .ReturnsAsync(book);
 
+        userRepo.Setup(r => r.IsFavoriteAsync(TestUserId, query.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expectedIsFavorite);
+
         var result = await handler.Handle(query, CancellationToken.None);
 
         result.Should().NotBeNull();
         result.Id.Should().Be(1);
         result.Title.Should().Be("Test Book");
+        result.IsFavorite.Should().Be(expectedIsFavorite);
     }
 
     [Fact]
